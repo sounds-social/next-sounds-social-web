f0b22587246a6c208be885cd7e2b0ccf17952330
Matteo Demicheli <matteo@matteodem.ch>
2023-08-04 14:17:50 +0200

    Add redirect on no user

---
+++ pages/sound/upload.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="py-6 px-4">
+    <h1 class="text-6xl mb-14 mt-8 text-slate-900 font-black">#Upload Sound</h1>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from "../../stores/auth";
+
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+if (!authStore.user) {
+  router.push({ path: "/" });
+}
+
+</script>
